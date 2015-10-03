d1d3d9515336679512f0028a096aaf6a521c72d8
hugo roman <hugo.roman@me.com>
2015-10-03 04:09:49 -0300

    Initial Commit

---
+++ MemeV2.01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeV2.01
+//
+//  Created by hugo roman on 10/3/15.
+//  Copyright © 2015 hugo roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
